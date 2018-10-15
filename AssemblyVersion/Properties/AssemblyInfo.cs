6cb3e5490af7e8568786e31ed5ce77e83362a560
Madhon <madhon@madhon.co.uk>
2018-10-15 17:14:02 +0100

    Not much

--- AssemblyVersion/Properties/AssemblyInfo.cs
+++ AssemblyVersion/Properties/AssemblyInfo.cs
@@ -7,7 +7,7 @@ using System.Runtime.InteropServices;
 [assembly : AssemblyConfiguration("")]
 [assembly : AssemblyCompany("Madhon")]
 [assembly:  AssemblyProduct("AssemblyVersionApp")]
-[assembly : AssemblyCopyright("Copyright © Madhon 2015")]
+[assembly : AssemblyCopyright("Copyright © Madhon 2018")]
 [assembly : AssemblyTrademark("")]
 [assembly : AssemblyCulture("")]
 
@@ -17,4 +17,4 @@ using System.Runtime.InteropServices;
 [assembly : Guid("e97bfa63-e8bc-41a8-90ce-52f4e7e31392")]
 
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.15064.1712")]
+[assembly: AssemblyFileVersion("1.0.18288.1712")]
